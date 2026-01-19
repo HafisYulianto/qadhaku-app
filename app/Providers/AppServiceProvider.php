ff0f3bfba6bbf369945dd4fab17e28d8036350be
HafisYulianto <hafis_yulianto@teknokrat.ac.id>
2026-01-20 04:46:35 +0700

    Fix tampilan HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Ini kodingan supaya CSS/Desain tidak berantakan di Railway (HTTPS)
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
